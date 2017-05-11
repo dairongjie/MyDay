c54a5e0d274591ef9b7186dae0a12aac3d6e3d6a
C515 <C515>
2017-05-11 15:56:06 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/myday/util/MainActivity.java
+++ app/src/main/java/com/example/myday/util/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.myday;
+package com.example.myday.util;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.myday.R;
 
 public class MainActivity extends AppCompatActivity {
 
